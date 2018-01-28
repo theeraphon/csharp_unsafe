d56b4899fcdc88262dfffe58ba1dc339b520c174
theeraphon <theeraphon.sa@gmail.com>
2018-01-29 01:01:11 +0700

    add copy string to string code

--- Unsafe_Code/Program.cs
+++ Unsafe_Code/Program.cs
@@ -6,7 +6,32 @@ namespace Unsafe_Code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string a, b = "";
+            a = "Test";
+            Console.WriteLine("a = {0}", a);
+            Console.WriteLine("b = {0}", b);
+            unsafe
+            {
+                fixed (char* sPnt = a, dPnt = b)
+                {
+                    Copy(sPnt, dPnt);
+                    b = new string(dPnt);
+                }
+            }
+            Console.WriteLine("a = {0}", a);
+            Console.WriteLine("b = {0}", b);
+        }
+
+        private static unsafe void Copy(char* src, char* dest)
+        {
+            char* t = dest;
+            while (*src != 0)
+            {
+                *t = *src;
+                src++;
+                t++;
+            }
+
         }
     }
 }
